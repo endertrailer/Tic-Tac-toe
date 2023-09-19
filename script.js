0ebd86a1daad5972679d9b1c4e3ca99802c66ef8
endertrailer <anismasoom80@gmail.com>
2023-09-19 17:40:40 +0530

    finish game logic!

--- script.js
+++ script.js
@@ -41,8 +41,38 @@ function gameBoard() {
   const switchPlayerTurn = () => {
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
   };
+  
   const getPlayerMark = () => activePlayer.token;
+
+  const player = document.querySelector('.player');
+
+  const changePlayer = () => {
+    if(activePlayer.token === 'X'){
+        player.textContent = "Player 1's Turn";
+    }
+    else if(activePlayer.token === 'O'){
+        player.textContent = "Player 2's Turn";
+    }
+  }
   let count = 0;
+  let continueGame = true;
+    function endGame(value){
+        if(value === 'end'){
+            if(activePlayer.token === 'O'){
+                player.textContent = 'Player 1 Wins!';
+                continueGame = false;
+            }
+            else if(activePlayer.token === 'X'){
+                player.textContent = "Player 2 Wins!";
+                continueGame = false;
+            }
+        }
+        else if(value === 'Tie'){
+            player.textContent = "It's a Tie!"
+            continueGame = false;
+        }
+    }
+
     function addMark(){
         const container = document.querySelector('.board');
         
@@ -55,13 +85,14 @@ function gameBoard() {
                     for(let j = 0; j < row; j++){
                     const cell = cells[cellNo];
                     cell.addEventListener('click', () => {
-                       if(cell.textContent === ''){
+                       if(cell.textContent === '' && continueGame === true){
                         cell.textContent = getPlayerMark();
                         board[i][j] = getPlayerMark();
                         count++;
                         console.log(count);
-                        getWinner(i,j);
                         switchPlayerTurn();
+                        changePlayer();
+                        getWinner(i,j);
                        }
                        
                     });
@@ -73,18 +104,17 @@ function gameBoard() {
     
     function getWinner(column, row){
         if(board[column][0] === board[column][1] && board[column][1] === board[column][2]){
-            console.log(`The winner is ${activePlayer.name}`);
-
+            endGame('end');
         }
         else if(board[0][row] === board[1][row] && board[1][row] === board[2][row]){
-            console.log(`The winner is ${activePlayer.name}`);
+            endGame('end');
         }
         else if(board[0][0] === board[1][1] && board[1][1]  && board[0][0] !== '' && board[1][1] === board[2][2] && board[2][2] !== '' || board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[1][1] !== '' && board[2][0] !== ''){
-            console.log(`The winner is ${activePlayer.name}`)
+            endGame('end');
 
         }
         else if(count === 9){
-            console.log('its a tie')
+            endGame('Tie');
         }
     }
 
